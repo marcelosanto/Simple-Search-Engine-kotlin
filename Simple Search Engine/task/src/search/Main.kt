78d45d6018fdfc262ffa1bc7a38701391a45f88b
Marcelo Santos <rocha.mer21@gmail.com>
2022-07-12 19:41:09 -0300

    Stage 2/6: Expand the search

--- Simple Search Engine/task/src/search/Main.kt
+++ Simple Search Engine/task/src/search/Main.kt
@@ -1,10 +1,42 @@
 package search
 
 fun main() {
-    val text = readln().split(" ").toMutableList()
-    val find = readln()
+    val listNames = mutableListOf<String>()
 
-    val search = text.indexOf(find)
+    while (true) {
+        println("Enter the number of people:")
+        val numberOfPeoples = readln().toInt()
 
-    if (search >= 0) println(search+1) else println("Not found")
+        println("Enter all people:")
+        repeat(numberOfPeoples) {
+            listNames.add(readln())
+        }
+
+        println("Enter the number of search queries:")
+        val numberOfSearchs = readln().toInt()
+
+
+        repeat(numberOfSearchs) {
+            println("Enter data to search people:")
+            val search = readln().lowercase()
+            searchNames(listNames, search)
+        }
+        break
+    }
+
+}
+
+private fun searchNames(listNames: List<String>, search: String) {
+    var count = 0
+    for (i in listNames.indices) {
+        if ("[a-zA-Z]*\\s*$search\\w*\\s*[a-zA-Z]*".toRegex().find(listNames[i].lowercase())?.value != null) {
+            if (count == 0) {
+                println("People found:")
+                println(listNames[i])
+                count++
+            } else println(listNames[i])
+        }
+    }
+
+    if (count == 0) println("No matching people found.")
 }
