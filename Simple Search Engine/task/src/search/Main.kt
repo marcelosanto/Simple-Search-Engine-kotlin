da834a4e96fae581795f2c1927a69cc70895a59c
Marcelo Santos <rocha.mer21@gmail.com>
2022-07-12 21:05:10 -0300

    Stage 4/6: X-files

--- Simple Search Engine/task/src/search/Main.kt
+++ Simple Search Engine/task/src/search/Main.kt
@@ -1,18 +1,23 @@
 package search
 
-fun main() {
+import java.io.File
+
+fun main(args: Array<String>) {
     val listNames = mutableListOf<String>()
 
-    while (true) {
-        if (listNames.isEmpty()) {
-            println("Enter the number of people:")
-            val numberOfPeoples = readln().toInt()
+    val fileName = args[1]
+    val file = File(fileName)
 
-            println("Enter all people:")
-            repeat(numberOfPeoples) {
-                listNames.add(readln())
-            }
-        }
+    while (true) {
+//        if (listNames.isEmpty()) {
+//            println("Enter the number of people:")
+//            val numberOfPeoples = readln().toInt()
+//
+//            println("Enter all people:")
+//            repeat(numberOfPeoples) {
+//                listNames.add(readln())
+//            }
+//        }
 
         println("=== Menu ===")
         println("1. Find a person")
@@ -23,33 +28,38 @@ fun main() {
 
         when (menuOpt) {
             "0" -> break
-            "1" -> findAPerson(listNames)
-            "2" -> printAllPeople(listNames)
+            "1" -> findAPerson(file)
+            "2" -> printAllPeople(file)
             else -> println("Incorrect option! Try again.")
         }
     }
 
 }
 
-fun findAPerson(listNames: MutableList<String>) {
+fun findAPerson(listNames: File) {
     println("Enter a name or email to search all suitable people.")
-    val nameOrEmail = readln()
+    val nameOrEmail = readln().lowercase()
     searchNames(listNames, nameOrEmail)
 }
 
-fun printAllPeople(listNames: MutableList<String>) {
+fun printAllPeople(listNames: File) {
     println("=== List of people ===")
-    for (i in listNames) {
-        println(i)
+    val lines = listNames.readLines()
+    for (line in lines) {
+        println(line)
     }
     println("")
 }
 
-private fun searchNames(listNames: List<String>, search: String) {
-    for (i in listNames.indices) {
-        if ("[a-zA-Z]*\\s*$search\\w*\\s*[a-zA-Z]*".toRegex().find(listNames[i].lowercase())?.value != null) {
-            println(listNames[i])
+private fun searchNames(listNames: File, search: String) {
+    if (listNames.exists()) { // checks if file exists
+        val lines = listNames.readLines()
+        for (i in lines) {
+            if ("[a-zA-Z]*\\s*@?$search@?\\w*\\s*[a-zA-Z]*".toRegex().find(i.lowercase())?.value != null) {
+                println(i)
+            }
         }
     }
+
     println("")
 }
